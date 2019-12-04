603fda5460f797d423d0cba4e83ec1d57932f166
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-12-03 21:50:19 -0500

    Added first route

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // implement your API here
+const express = require("express");
+let db = require("./data/db");
+
+const app = express();
+
+app.use(express.json());
+
+//routes
+app.get("/api/users", (req, res) => {
+  res.json(db);
+});
+
+// server start
+const port = 8080;
+const host = "127.0.0.1";
+
+app.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}`);
+});
