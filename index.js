21880f805f8515cfc5bdf5300f74857933b8e2e8
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-12-03 22:03:02 -0500

    Added the route to post users

--- index.js
+++ index.js
@@ -8,7 +8,26 @@ app.use(express.json());
 
 //routes
 app.get("/api/users", (req, res) => {
-  res.json(db);
+  //   res.json(db);
+  db.find();
+});
+
+app.post("/api/users", (req, res) => {
+  if (!req.body.name || !req.body.bio) {
+    return res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  }
+
+  const newUser = {
+    name: req.body.name,
+    bio: req.body.bio,
+    created_at: Date.now(),
+    updated_at: Date.now()
+  };
+
+  db.insert(newUser);
+  res.status(201).json(newUser);
 });
 
 // server start
